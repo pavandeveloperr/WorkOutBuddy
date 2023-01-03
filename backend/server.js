29fbeb760835272a2e6e4ed252ce755db58c7ef6
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-03 22:57:10 +0530

    setup mongodb & connect to db using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const express =  require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+
 
 //express app
 const app = express();
@@ -16,7 +18,16 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+        console.log('connect to db & listening on port', process.env.PORT)
 })
+
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
