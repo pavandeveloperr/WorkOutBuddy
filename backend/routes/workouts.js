ab8f0aca843fb6c8e83fcbe09b5a1652143c7668
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-04 12:02:39 +0530

    create model and collections

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const Workout = require('../models/workoutModel')
 const router = express.Router()
 
 // GET all workouts
@@ -13,8 +13,15 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a workout
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a workout'})
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+
+    try {
+        const workout = await Workout.create({title, load, reps})  // async func
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 })
 
 // DELETE a workout
