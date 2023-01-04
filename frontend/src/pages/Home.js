00a70cd64daf060dfecab3249e4079eec1046df6
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-04 16:56:52 +0530

    feat : create workoutDetails component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 
+// components
+import WorkoutDetails from "../components/workoutDetails";
+
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
 
@@ -19,7 +22,9 @@ const Home = () => {
     <div className="home">
       <div className="workouts">
         {workouts &&
-          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
       </div>
     </div>
   );
