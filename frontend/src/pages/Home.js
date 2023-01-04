0631ecd8ac9b081cb19c4bd050592aa704ccc412
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-01-04 18:31:44 +0530

    feat : create form to add workouts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
 // components
-import WorkoutDetails from "../components/workoutDetails";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -26,6 +27,7 @@ const Home = () => {
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutForm />
     </div>
   );
 };
