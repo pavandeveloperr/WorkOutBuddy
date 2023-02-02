1ec8c1055234b9ddbd130d6a89bd47e2e57643dd
Pavan Kulkarni <pavankulkarniraichur@gmail.com>
2023-02-02 16:06:40 +0530

    deployment setup

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 const express =  require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
-PORT =  process.env.PORT || 3000
-
+const path = require('path')
 
 //express app
 const app = express();
@@ -19,12 +18,27 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
+
+// Deployment
+__dirname = path.resolve();
+if(process.env.NODE === 'production') {
+    app.use(express.static(path.join(__dirname, "frontend/build")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running...")
+    })
+}
+
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for requests
-        app.listen(PORT, () => {
-        console.log('connect to db & listening on port', PORT)
+        app.listen(process.env.PORT, () => {
+        console.log('connect to db & listening on port', process.env.PORT)
 })
 
     })
